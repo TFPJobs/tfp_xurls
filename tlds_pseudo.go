87e351cbb097d5b6a08db0bdf704c1505eb21764
TFPJobs <martindell90@gmail.com>
2022-10-22 23:21:35 -0700

    first commit

--- tlds_pseudo.go
+++ tlds_pseudo.go
@@ -11,14 +11,5 @@ package xurls
 //   - https://tools.ietf.org/html/draft-grothoff-iesg-special-use-p2p-names-00
 //   - https://www.iana.org/assignments/special-use-domain-names/special-use-domain-names.xhtml
 var PseudoTLDs = []string{
-	`bit`,       // Namecoin
-	`example`,   // Example domain
-	`exit`,      // Tor exit node
-	`gnu`,       // GNS by public key
-	`i2p`,       // I2P network
-	`invalid`,   // Invalid domain
-	`local`,     // Local network
-	`localhost`, // Local network
-	`test`,      // Test domain
-	`zkey`,      // GNS domain name
+	`bit`  
 }
